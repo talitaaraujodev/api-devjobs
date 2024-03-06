83b43a84308bf3fe722e493efe134809987aa7a4
Talita Araujo <74464709+talitaaraujodev@users.noreply.github.com>
2024-03-06 15:29:01 -0300

    Include script

--- src/shared/http/server.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import app from './App';
-import { usersRoutes } from '../../adapter/input/routes/usersRoute';
-import { profilesRoutes } from '../../adapter/input/routes/profilesRoute';
-import { authRoutes } from '../../adapter/input/routes/authRoute';
-import envConfig from '../../config/envConfig';
-
-app.listen(envConfig.serverPort, [usersRoutes, profilesRoutes, authRoutes]);
